f73271bef2fb1f39bf2cf8391fb31c3587bc6f40
Gabarros <gabarros17@gmail.com>
2020-02-13 20:33:31 -0300

    create redux and saga configuration

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
 import './config/ReactotronConfig';
@@ -6,14 +7,19 @@ import './config/ReactotronConfig';
 import Routes from './routes';
 import history from './services/history';
 
+import store from './store';
+
 import GlobalStyle from './styles/global';
 
 export default function App() {
   return (
-    <Router history={history}>
+    <Provider store={store}>
+      <Router history={history}>
       <GlobalStyle />
       <Routes />
     </Router>
+    </Provider>
+    
     
   )
 }
